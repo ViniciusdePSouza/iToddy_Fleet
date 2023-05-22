17a1ed6a3c006a600cedefa28df68692d7899f1d
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2023-05-22 12:25:29 -0300

    Firt commit

---
+++ iToddyfleet/App.tsx
@@ -0,0 +1,14 @@
+import { SignIn } from './src/screens/SignIn';
+
+import { ThemeProvider } from 'styled-components/native'
+
+import theme from './src/theme';
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <SignIn/>
+    </ThemeProvider>
+  );
+}
+
